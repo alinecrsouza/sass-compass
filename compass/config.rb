a5d027207804e321e1ca09393131013cb3886e07
alinecrsouza <linecrsouza@gmail.com.br>
2016-05-24 11:03:37 -0300

    Learning about Helpers:image-url.

--- compass/config.rb
+++ compass/config.rb
@@ -2,7 +2,7 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = ""
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
@@ -13,7 +13,7 @@ javascripts_dir = "js"
 output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
