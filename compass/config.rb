01f3ad03bc9df6294d47c98219573f78e472228b
alinecrsouza <linecrsouza@gmail.com.br>
2016-06-01 11:49:32 -0300

    Learning about Sprites.

--- compass/config.rb
+++ compass/config.rb
@@ -1,8 +1,7 @@
-require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = ""
+http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
